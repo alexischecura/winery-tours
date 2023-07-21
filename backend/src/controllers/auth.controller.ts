395f9f276f52a5200f148b164c324f2611082540
Alexis Checura <alexischecura@gmail.com>
2023-07-21 12:52:12 -0300

    feat/add verification authenticated session for users

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { CookieOptions, NextFunction, Request, Response } from 'express';
 import { Prisma } from '@prisma/client';
+import crypto from 'crypto';
 import bcrypt from 'bcryptjs';
 
 import { envVars } from '../configs/env.config';
@@ -7,9 +8,12 @@ import { CreateUserType, LoginUser } from '../schemas/user.schema';
 import { createUser, getUser, signTokens } from '../services/user.service';
 import {
   AuthenticationError,
+  AuthorizationError,
   ConflictError,
   InternalServerError,
 } from '../utils/AppError';
+import { verifyJwt } from '../utils/jwtUtils';
+import { redisClient } from '../databases/redis.db';
 
 // Cookies Configurations
 
@@ -41,11 +45,18 @@ export const createUserHandler = async (
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
+    const verifyCode = crypto.randomBytes(32).toString('hex');
+    const verificationCode = crypto
+      .createHash('sha256')
+      .update(verifyCode)
+      .digest('hex');
+
     // Create new user
     const newUser = await createUser({
       name,
-      email,
+      email: email.toLocaleLowerCase(),
       password: hashedPassword,
+      verificationCode,
     });
 
     res.status(201).json({
@@ -78,8 +89,6 @@ export const loginUserHandler = async (
   try {
     const { email, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 12);
-
     // Get the user
     const user = await getUser(
       { email: email.toLowerCase() },
@@ -92,12 +101,81 @@ export const loginUserHandler = async (
 
     const { access_token, refresh_token } = await signTokens(user);
 
+    res.cookie('access_token', access_token, accessTokenCookieOptions);
     res.cookie('refresh_token', refresh_token, refreshTokenCookieOptions);
     res.cookie('logged_in', true, {
       ...accessTokenCookieOptions,
       httpOnly: false,
     });
+
+    res.status(200).json({
+      status: 'success',
+      access_token,
+    });
+  } catch (error) {
+    console.log(error);
+    next(new InternalServerError('Something went wrong.'));
+  }
+};
+
+export const authenticateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    let access_token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      access_token = req.headers.authorization.split(' ').at(1);
+    }
+    if (!access_token)
+      return next(new AuthenticationError('You are not logged'));
+
+    const decoded = verifyJwt<{ sub: string }>(
+      access_token,
+      'ACCESS_TOKEN_PUBLIC_KEY'
+    );
+    if (!decoded)
+      return next(
+        new AuthenticationError("Invalid token or user doesn't exist")
+      );
+
+    const session = await redisClient.get(decoded.sub);
+    if (!session)
+      return next(new AuthenticationError('Invalid token or session expired'));
+
+    const user = await getUser({ id: JSON.parse(session).id });
+    if (!user)
+      return next(new AuthenticationError('Invalid token or session expired'));
+
+    const localUser = {
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+
+    res.locals.user = localUser;
   } catch (error) {
     next(new InternalServerError('Something went wrong.'));
+  }
+};
+
+export const refreshAccessTokenHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const refresh_token = req.cookies.refresh_token;
+
+    if (!refresh_token) {
+      return next(new AuthorizationError('Failed to refresh access token'));
+    }
+  } catch (error) {
+    next(error);
   }
 };
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { createUserSchema } from '../schemas/user.schema';
-import { validateBody } from '../middlewares/validators';
-import { createUserHandler } from '../controllers/auth.controller';
+import { createUserSchema, loginUserSchema } from '../schemas/user.schema';
+import { validateBody } from '../schemas/validators';
+import {
+  createUserHandler,
+  loginUserHandler,
+} from '../controllers/auth.controller';
 
 const router = Router();
 
 router.post('/signup', validateBody(createUserSchema), createUserHandler);
+router.post('/login', validateBody(loginUserSchema), loginUserHandler);
 
 export default router;
