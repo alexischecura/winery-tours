1253f532d594fd45dfc02ab7ff5e7d04b5ae98a3
Alexis Checura <alexischecura@gmail.com>
2023-07-25 10:36:59 -0300

    feat/add rate limit requests middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
 
 import { envVars } from './configs/env.config';
-import { NotFoundError } from './utils/AppError';
+import { NotFoundError, RateLimitError } from './utils/AppError';
 import globalErrorHandler from './controllers/error.controller';
 import wineriesRouter from './routes/wineries.routes';
 import toursRouter from './routes/tours.routes';
@@ -25,10 +26,28 @@ if (envVars.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests
+const limiter = rateLimit({
+  max: 5,
+  handler: () => {
+    throw new RateLimitError(
+      `Too many request from this IP, try again in ${envVars.RATE_LIMIT_TIME_IN_SECONDS} seconds`
+    );
+  },
+  windowMs: envVars.RATE_LIMIT_TIME_IN_SECONDS * 1000,
+});
+app.use(limiter);
+
 // Routes
 app.use('/api/v1/wineries', wineriesRouter);
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
+app.get('/test', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to the winery-tours API',
+  });
+});
 app.all('*', (req, _, next) => {
   next(new NotFoundError(req.originalUrl));
 });
--- backend/src/configs/env.config.ts
+++ backend/src/configs/env.config.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']),
   PORT: z.string().regex(numberRegex).transform(Number),
   ORIGIN: z.string(),
+  RATE_LIMIT_TIME_IN_SECONDS: z.string().regex(numberRegex).transform(Number),
 
   POSTGRES_VERSION: z.string(),
   POSTGRES_USER: z.string(),
