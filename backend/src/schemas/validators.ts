cd63a243e912e13b51dffe19d754f0125cf2b991
Alexis Checura <alexischecura@gmail.com>
2023-07-29 11:17:54 -0300

    feat/add params validator

--- backend/src/schemas/validators.ts
+++ backend/src/schemas/validators.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { z } from 'zod';
-import { ValidationError } from '../utils/AppError';
+import { InternalServerError, ValidationError } from '../utils/AppError';
 
 export const validateBody =
   (schema: z.Schema) => (req: Request, res: Response, next: NextFunction) => {
@@ -13,8 +13,35 @@ export const validateBody =
           field: err.path.at(0),
           message: err.message,
         }));
-        next(new ValidationError('Error validating the data', errors));
+        next(
+          new ValidationError('Error validating the data from body', errors)
+        );
       }
-      next(error);
+      next(
+        new InternalServerError('Something went wrong when validation the body')
+      );
+    }
+  };
+
+export const validateParams =
+  (schema: z.Schema) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.params);
+      next();
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const errors = error.errors.map((err: z.ZodIssue) => ({
+          field: err.path.at(0),
+          message: err.message,
+        }));
+        next(
+          new ValidationError('Error validating the data from params', errors)
+        );
+      }
+      next(
+        new InternalServerError(
+          'Something went wrong when validation the params'
+        )
+      );
     }
   };
