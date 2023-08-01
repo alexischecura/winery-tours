65e1ac1f3f20193e281b281dec19a68a55a20c5e
Alexis Checura <alexischecura@gmail.com>
2023-08-01 10:48:08 -0300

    feat/add react-query, react-hot-toast dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import LandingPage from './pages/LandingPage';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster />
+      </QueryClientProvider>
     </>
   );
 }
