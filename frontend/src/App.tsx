0d0999a3486ed0f56cbacbdf502af471d03d051f
Alexis Checura <alexischecura@gmail.com>
2023-08-03 20:05:13 -0300

    feat/add login route and adapt some styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,6 +22,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<LandingPage />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
       <Toaster />
