90524f7d05c175156edae128f987f91a43ecf217
Alexis Checura <alexischecura@gmail.com>
2023-08-07 17:43:39 -0300

    feat/complete all api auth services

--- frontend/src/services/apiAuth.ts
+++ frontend/src/services/apiAuth.ts
@@ -11,56 +11,67 @@ enum AuthUrls {
   SIGNUP = `${API_AUTH_URL}/signup`,
   LOGIN = `${API_AUTH_URL}/login`,
   LOGOUT = `${API_AUTH_URL}/logout`,
+  VERIFICATION = `${API_AUTH_URL}/verification`,
+  ME = `${API_AUTH_URL}/me`,
 }
 
-export const signUpUser = async (
-  credetials: SingUpUser
-): Promise<GenericResponse> => {
-  const res = await fetch(AuthUrls.SIGNUP, {
-    method: 'POST',
+enum Methods {
+  GET = 'GET',
+  POST = 'POST',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
+
+async function fetchApi<T>(
+  url: string,
+  method: Methods,
+  body?: object
+): Promise<T> {
+  const res = await fetch(url, {
+    method,
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(credetials),
+    body: JSON.stringify(body),
   });
+
   if (res.ok) {
-    return (await res.json()) as GenericResponse;
+    return (await res.json()) as T;
   } else {
     const data = (await res.json()) as ErrorResponse;
     throw data;
   }
+}
+
+export const signUpUser = async (
+  credentials: SingUpUser
+): Promise<GenericResponse> => {
+  return fetchApi<GenericResponse>(AuthUrls.SIGNUP, Methods.POST, credentials);
 };
 
 export const loginUser = async (
   email: string,
   password: string
 ): Promise<LoginUserResponse> => {
-  const res = await fetch(AuthUrls.LOGIN, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
+  return fetchApi<LoginUserResponse>(AuthUrls.LOGIN, Methods.POST, {
+    email,
+    password,
   });
-  if (res.ok) {
-    return (await res.json()) as LoginUserResponse;
-  } else {
-    const data = (await res.json()) as ErrorResponse;
-    throw data;
-  }
 };
 
-export const logoutUser = async () => {
-  const res = await fetch(AuthUrls.LOGOUT, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  if (res.ok) {
-    return (await res.json()) as GenericResponse;
-  } else {
-    const data = (await res.json()) as ErrorResponse;
-    throw data;
-  }
+export const verificateUser = async (
+  verificationCode: string
+): Promise<GenericResponse> => {
+  return fetchApi<GenericResponse>(
+    `${AuthUrls.VERIFICATION}/${verificationCode}`,
+    Methods.POST
+  );
+};
+
+export const logoutUser = async (): Promise<GenericResponse> => {
+  return fetchApi<GenericResponse>(AuthUrls.LOGOUT, Methods.POST);
+};
+
+export const getMe = async (): Promise<GenericResponse> => {
+  return fetchApi<GenericResponse>(AuthUrls.ME, Methods.GET);
 };
