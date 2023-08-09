e3e277767f8a98798c37b6093a1aba594f78b332
Alexis Checura <alexischecura@gmail.com>
2023-08-08 21:15:01 -0300

    refactor/rename the login css file to form for general use

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,11 @@
-import styles from './Login.module.css';
+import styles from './Form.module.css';
 import LoginForm from '../features/authentication/LoginForm';
 import Logo from '../ui/Logo';
 import Heading from '../ui/Heading';
 
 function Login() {
   return (
-    <main className={styles.loginForm}>
+    <main className={styles.form}>
       <Logo />
       <Heading type='h3'>Login in your account</Heading>
       <LoginForm />
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,11 +1,11 @@
-import styles from './Login.module.css';
+import styles from './Form.module.css';
 import SignUpForm from '../features/authentication/SignUpForm';
 import Logo from '../ui/Logo';
 import Heading from '../ui/Heading';
 
 function SignUp() {
   return (
-    <main className={styles.loginForm}>
+    <main className={styles.form}>
       <Logo />
       <Heading type='h3'>Create a new account</Heading>
       <SignUpForm />
