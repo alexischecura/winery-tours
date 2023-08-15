e010435ed310e162fd7b709ccfd51029d203bb1b
Alexis Checura <alexischecura@gmail.com>
2023-08-15 18:54:32 -0300

    feat/add accessToken when querying the user

--- frontend/src/features/authentication/useUser.ts
+++ frontend/src/features/authentication/useUser.ts
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCurrentUser } from '../../services/apiAuth';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export function useUser() {
+  const { accessToken } = useContext(AuthContext);
+
   const { isLoading, data } = useQuery({
     queryKey: ['currentUser'],
-    queryFn: getCurrentUser,
+    queryFn: () => getCurrentUser(accessToken),
   });
 
   return { isLoading, data };
