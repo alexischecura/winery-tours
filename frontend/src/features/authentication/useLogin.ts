e334ace204b9069bde8f64056e59fb6f1b676590
Alexis Checura <alexischecura@gmail.com>
2023-08-15 18:52:00 -0300

    feat/add setters for isLoggedIn and accessToken

--- frontend/src/features/authentication/useLogin.ts
+++ frontend/src/features/authentication/useLogin.ts
@@ -4,16 +4,19 @@ import { loginUser } from '../../services/apiAuth';
 import { ErrorResponse } from '../../types/types';
 import { toast } from 'react-hot-toast';
 import { LoginUserType } from '../../types/userTypes';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export function useLogin() {
-  const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const { setIsLoggedIn, setAccessToken } = useContext(AuthContext);
 
   const { mutate: loginApi, isLoading } = useMutation({
     mutationFn: ({ email, password }: LoginUserType) =>
       loginUser(email, password),
     onSuccess: (user) => {
-      queryClient.setQueryData(['user'], user);
+      setIsLoggedIn(true);
+      setAccessToken(user.access_token);
       navigate('/dashboard', { replace: true });
     },
     onError: (error: ErrorResponse) => {
